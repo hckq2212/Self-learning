0469eba30480499c6d64e0de13567562a97dd0bb
quangkid2212 <163752892+hckq2212@users.noreply.github.com>
2025-06-06 21:47:24 +0700

    learn to use morgan

--- BE/Framework/Express.js/3.4 Middleware/index1.js
+++ BE/Framework/Express.js/3.4 Middleware/index1.js
@@ -2,11 +2,13 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import morgan from "morgan";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(morgan('combined'));
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
 
--- BE/Framework/Express.js/3.4 Middleware/index3.js
+++ BE/Framework/Express.js/3.4 Middleware/index3.js
@@ -1,9 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
 
-app.use(logger);
+app.use(morgan('combined'))
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.name);
+  next()
+})
 
 app.get("/", (req, res) => {
   res.send("Hello");
